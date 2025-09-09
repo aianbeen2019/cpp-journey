228a4c28d8fbf414cd59116fd0b4f5e4b12feb5e
Ian Beenblossom <aianbeen_2019@outlook.com>
2025-09-09 08:06:31 -0700

    Update Optimization(Compile_time_Programming\basic).cpp

--- LearnCPP_Journey/Optimization(Compile_time_Programming\basic).cpp
+++ LearnCPP_Journey/Optimization(Compile_time_Programming\basic).cpp
@@ -2,3 +2,31 @@
 
 #include <iostream>
 
+//compile-time constants
+constexpr int baseHealth{ 20 };
+constexpr int baseMana{ 20 };
+constexpr int baseStrength{ 5 };
+constexpr int baseDexterity{ 5 };
+constexpr int baseIntelligence{ 5 };
+//compile-time constants
+
+//compile-time function
+constexpr int calculateAttackPower(int strength, int dexterity) {
+  return (strength * 2) + (dexterity / 2);
+}
+
+
+int main()
+{
+  //Evaluated during comile-time 
+  constexpr int attackPower = calculateAttackPower(baseStrength, baseDexterity);
+
+  std::cout << "Base Health" << baseHealth << '\n';
+  std::cout << "Base Mana" << baseMana << '\n';
+  std::cout << "Attack Power" << attackPower << '\n';
+ 
+  return 0;
+  
+}
+
+
