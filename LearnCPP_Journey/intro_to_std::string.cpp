e853303ccfd5f6e5e2ecabb96a57fd78e773a31e
Ian Beenblossom <aianbeen_2019@outlook.com>
2025-09-09 18:33:17 -0700

    Update intro_to_std::string.cpp

--- LearnCPP_Journey/intro_to_std::string.cpp
+++ LearnCPP_Journey/intro_to_std::string.cpp
@@ -4,3 +4,38 @@
 #include <string>
 
 
+//compile-time constants
+constexpr int baseHealth{ 20 };
+constexpr int baseMana{ 20 };
+constexpr int baseStrength{ 5 };
+constexpr int baseDexterity{ 5 };
+constexpr int baseIntelligence{ 5 };
+//compile-time constants
+
+//compile-time function
+constexpr int calculateAttackPower(int strength, int dexterity) {
+  return (strength * 2) + (dexterity / 2);
+}
+
+
+int main()
+{
+
+  std::string playerName;
+  std::cout << "Enter character's name." << '\n';
+  std::getline(std::cin, playerName);
+  
+  
+
+  //Evaluated during comile-time 
+  constexpr int attackPower = calculateAttackPower(baseStrength, baseDexterity);
+
+  std::cout << "\n---Character Sheet---\n";
+  std::cout << "Hero: " << playerName << '\n';
+  std::cout << "Health: " << baseHealth << '\n';
+  std::cout << "Base: " << baseMana << '\n';
+  std::cout << "Attack: " << attackPower << '\n';
+ 
+  return 0;
+  
+}
